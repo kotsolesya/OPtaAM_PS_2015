c33bb09f74182097240cc407538cfc8ffd4bf2a3
EdwardUA <ANANASFAMILEVICH@gmail.com>
2017-10-18 09:47:53 +0300

    Практические 5-9

---
+++ Gorelik_Eduard/prakti4na 7/2 zadanie.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+//#include <ifstream.h>
+#include <iostream>
+const int N =5;
+using namespace std;
+ main()
+{ int a[N];
+       ofstream out("numbers.txt",ios_base::app);
+if (!out) {
+cout << "Cannot open file.\n";
+return 1;
+}
+for(int i=0;i<N;i++){
+cin >> a[i];
+        }
+for(int i=0;i<N;i++){
+out << a[i] << "\n";}
+out.close ();
+
+system ("PAUSE"); 
+return 0;
+
+}
+
+
---
+++ Gorelik_Eduard/prakti4na 9/robiv sam no proval/csv file creator (zadanie 1) 15-16. 10 17(popitka 2) sam pitalsya.cpp
@@ -0,0 +1,76 @@
+#include <stdio.h>//for eof()
+#include<iostream>
+#include<string.h>
+#include<cstdlib> // dlya fynk  strtol
+#include <fstream>
+const int N=4;
+using namespace std;
+struct Tpochta{
+char namehuman[100];
+char lastname[100];
+char email[100];
+char password[30];
+}human;
+
+
+char  createFile_addFieldToFile(char *name)
+{ofstream vvod_from_file(name, ios_base::app);
+cout<<"Vvedite name (char)"<<"=";
+cin>>human.namehuman;
+cout<<"Vvedite lastname(char)"<<"=";
+cin>>human.lastname;
+cout<<"Vvedite email(char)"<<"=";
+cin>>human.email;
+cout<<"Vvedite password(char)"<<"=";
+cin>>human.password;
+vvod_from_file<<"human.namehuman "<<human.namehuman<<";"<<endl;
+vvod_from_file<<"human.lastname "<<human.lastname<<";"<<endl;
+vvod_from_file<<"human.email "<<human.email<<";"<<endl;
+vvod_from_file<<"human.password "<<human.password<<";"<<endl;
+cout<<"End createFile()"<<endl;
+vvod_from_file.close();
+}
+
+char  validEmai(char *name)
+{ofstream vvod_from_file(name, ios_base::app);
+	char str[] = "adress@domen.su";
+bool valid = true;
+int all_presents = 0;
+std::string suitable_symbols = "-_.@"; // допустимые символы помимо латинских букв и цифр
+for(int i = 0;str[i] != '\0'; i++){
+    if( !isalpha(str[i]) && !isdigit(str[i]) && suitable_symbols.find(str[i]) == std::string::npos){ // наличие недопустимого символа
+        valid = false; break;
+    }
+    if(i > 0 && str[i] == '.' && str[i - 1] == '.') { valid = false; break; } // две точки подряд
+    else if(i > 0 && str[i] == '@' && !all_presents) { all_presents = 1; } // наличие @
+    else if(str[i] == '@' && all_presents) { valid = false; break; } // наличие двух @ в строчке
+    else if(isalpha(str[i]) && isdigit(str[i]) && all_presents == 1) { all_presents = 2; } // наличие буквы или цифры после @
+    else if(str[i] == '.' && all_presents == 2) { all_presents = 3; } // наличие точки после символа( который после @ )
+    else if(isalpha(str[i]) && all_presents == 3 && str[i + 1] == '\0') { all_presents = 4; } // наличие буквы в конце строчки
+}
+if(valid && all_presents == 4) { cout << "adress is valid and contains all the needed parts."; }
+}
+
+/*
+char readFile(char *name)
+{ifstream vvod_from_file(name);  //NE ROBIT PO4EMUTO
+char a;
+
+ while (!name.eof()){ //Проходим по всему файлу, сдвигая указатель посимвольно
+
+                  a+=name.get();   
+				      
+      }
+      return a;
+
+}
+
+ */
+main(){
+createFile_addFieldToFile("Dani.csv");
+//readFile("Dani.csv");
+
+
+       system("pause");
+       return 0;
+       }
