c0506d77563fa5335c7f336f7c9518b27b7676d3
YarmakOleksndr <kaniv2011@gmail.com>
2017-10-18 09:33:07 +0300

    Add files

---
+++ Yarmosh_Oleksandr/Practical Work№4/3.2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <string.h>
+using namespace std;
+int main(){
+  char input [256];
+  cout << "Write number: ";
+  cin >> input;
+ 
+  int value = atoi(input);
+
+    cout << value << endl;
+	system("pause");
+
+}
