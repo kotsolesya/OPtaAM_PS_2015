a553fafb70f963c72a5f2db3629e924e48ab5515
Flat1488 <yegor.ka8@mail.ru>
2017-11-16 09:57:35 +0200

    Add some works

---
+++ Varich/1 laba/2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string.h>
+#include<cstdlib>
+using namespace std;
+main(){
+char a[]={"rocesorp"};
+char numbers[] ;
+cin>>a;
+cout<<a[7];
+for(int i=0;i<7;i++)
+{
+cout<<a[i];}
+cout<<endl;      
+for(int i=0;i<9;i++)
+{cout<<a[i];}
+system("pause");
+return 0;
+}
