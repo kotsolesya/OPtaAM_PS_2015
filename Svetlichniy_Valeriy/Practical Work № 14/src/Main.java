831e44dee25ec785c73c288a0bc22be9984e1494
VALER1Y <stexa62@hotmail.com>
2017-12-10 21:36:43 +0200

    Pracktick Work # 14 (Task 1.2,3)

---
+++ Svetlichniy_Valeriy/Practical Work № 14/src/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Point_One.You_Name();
+        Point_Two.You_Password();
+        Point_Three.Date();
+        MassClass.Mass();
+    }
+}
